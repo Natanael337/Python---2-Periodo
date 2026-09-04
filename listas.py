bef8e160cf492929add58a99a5116efca9dab705
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-09-04 19:20:16 -0300

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,13 @@
+valores = [10, 20, 30, 40, 50, 60]
+print('Na posição 1 está {}'.format(valores[1]))
+valores[1] = 25
+print('Na posição 1 está {}'.format(valores[1]))
+print(valores)
+
+print(valores[1:5])
+
+print(valores[:3])
+print(valores[3:])
+print(valores[::2])
+print(valores[-1])
+print(valores[::-1])
