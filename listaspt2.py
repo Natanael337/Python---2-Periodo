7fe8db0713d30777f25604b67aa958d5ad24c0ac
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-09-04 19:26:12 -0300

    Create listaspt2.py

---
+++ listaspt2.py
@@ -0,0 +1,11 @@
+tarefas = ['estudar', 'revisar']
+print(tarefas)
+#como inserir um item dentro de uma lista
+tarefas.append('praticar')
+print(tarefas)
+#como inserir varios itens dentro de uma lista, usando outra lista
+tarefas.extend(['testar', 'documentar'])
+print(tarefas)
+#eu consigo inserir um item em uma determinada posição
+tarefas.insert(1,'planejar')
+print(tarefas)
